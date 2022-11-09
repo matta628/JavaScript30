510842fd18189ef837abf9068a6a9350e9f4d827
Matt <matta628@gmail.com>
2022-11-09 13:01:24 -0500

    Refactored anonymous functions to named

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,4 +1,4 @@
-document.addEventListener('keydown', (event) => {
+function playSound(event){
     if (event.key.length > 1) return;
     let keyCode = event.key.toUpperCase().charCodeAt(0);
     const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
@@ -9,12 +9,14 @@ document.addEventListener('keydown', (event) => {
 
     const key = document.querySelector(`.key[data-key="${keyCode}"]`);
     key.classList.add('playing');
-});
+}
+
+function removeTransition(event){
+    if (event.propertyName != 'transform') return;
+    this.classList.remove('playing');
+}
 
 const keys = document.querySelectorAll('.key');
-keys.forEach(key => {
-    key.addEventListener('transitionend', (event) => {
-        if (event.propertyName != 'transform') return;
-        key.classList.remove('playing');
-    })
-});
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+document.addEventListener('keydown', playSound);
+
