f48ffcf5e2305cd722efa0c36ccc89caf6221949
Matt <matta628@gmail.com>
2022-11-09 12:56:39 -0500

    Add sound and transition to keys

---
+++ 01 - JavaScript Drum Kit/script.js
@@ -0,0 +1,20 @@
+document.addEventListener('keydown', (event) => {
+    if (event.key.length > 1) return;
+    let keyCode = event.key.toUpperCase().charCodeAt(0);
+    const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+    if (!audio) return;
+
+    audio.currentTime = 0;
+    audio.play();
+
+    const key = document.querySelector(`.key[data-key="${keyCode}"]`);
+    key.classList.add('playing');
+});
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => {
+    key.addEventListener('transitionend', (event) => {
+        if (event.propertyName != 'transform') return;
+        key.classList.remove('playing');
+    })
+});
